d400850cb76d54baf64bec46cba3a537a337494b
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-05-24 17:47:12 +0300

    updated app initialization on step#10

--- src/init.jsx
+++ src/init.jsx
@@ -3,7 +3,7 @@
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 import { io } from 'socket.io-client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 import store from './store.js';
@@ -13,17 +13,14 @@ import SocketProvider from './components/SocketProvider.jsx';
 
 import i18n from './components/i18n.js';
 
-export default async (socketClient = io()) => {
-
-  return (
-    <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <AuthProvider>
-          <SocketProvider socket={socketClient}>
-            <App />
-          </SocketProvider>
-        </AuthProvider>
-      </I18nextProvider>
-    </Provider>
-  );
-};
+export default async (socketClient = io()) => (
+  <Provider store={store}>
+    <I18nextProvider i18n={i18n}>
+      <AuthProvider>
+        <SocketProvider socket={socketClient}>
+          <App />
+        </SocketProvider>
+      </AuthProvider>
+    </I18nextProvider>
+  </Provider>
+);
