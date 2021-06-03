4f132fc7d9e799e7b54c2620c6f0ba8178827911
Danila Prokoshev <danilaprokoshev@gmail.com>
2021-06-03 22:08:51 +0300

    fixed modal handling on check#2

--- src/components/modals/CustomModal.jsx
+++ src/components/modals/CustomModal.jsx
@@ -15,108 +15,210 @@ import { useTranslation } from 'react-i18next';
 import useAuth from '../../hooks/authorization.jsx';
 import useSocket from '../../hooks/socket.jsx';
 
-const CustomModal = ({ modal, onHide }) => {
-  const renderSettingsByType = {
-    adding: (isOpened, onHideHandler, t, formik, inputRef) => {
-      return (
-        <Modal show={isOpened} onHide={onHideHandler}>
-          <Modal.Header closeButton>
-            <Modal.Title>{t('modals.adding.title')}</Modal.Title>
-          </Modal.Header>
+const renderSettingsByType = {
+  adding: (isOpened, onHideHandler, t, formik, inputRef) => {
+    return (
+      <Modal show={isOpened} onHide={onHideHandler}>
+        <Modal.Header closeButton>
+          <Modal.Title>{t('modals.adding.title')}</Modal.Title>
+        </Modal.Header>
 
-          <Modal.Body>
-            <Form noValidate onSubmit={formik.handleSubmit}>
-              <FormGroup>
-                <FormControl
-                  required
-                  ref={inputRef}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.body}
-                  data-testid="add-channel"
-                  isInvalid={formik.errors.body}
-                  name="body"
-                />
-                <FormControl.Feedback
-                  type="invalid"
-                >
-                  {formik.errors.body}
-                </FormControl.Feedback>
-              </FormGroup>
-              <div className="d-flex justify-content-end">
-                <Button className="mr-2 btn btn-secondary" onClick={onHideHandler}>
-                  {t('modals.adding.cancel')}
-                </Button>
-                <Button type="submit" variant="dark">
-                  {t('modals.adding.send')}
-                </Button>
-              </div>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      );
-    },
-    renaming: (isOpened, onHideHandler, t, formik, inputRef) => {
-      return (
-        <Modal show={isOpened} onHide={onHideHandler}>
-          <Modal.Header closeButton>
-            <Modal.Title>{t('modals.renaming.title')}</Modal.Title>
-          </Modal.Header>
-
-          <Modal.Body>
-            <Form noValidate onSubmit={formik.handleSubmit}>
-              <FormGroup>
-                <FormControl
-                  required
-                  ref={inputRef}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.body}
-                  data-testid="rename-channel"
-                  isInvalid={formik.errors.body}
-                  name="body"
-                />
-                <FormControl.Feedback
-                  type="invalid"
-                >
-                  {formik.errors.body}
-                </FormControl.Feedback>
-              </FormGroup>
-              <div className="d-flex justify-content-end">
-                <Button className="mr-2 btn btn-secondary" onClick={onHideHandler}>
-                  {t('modals.renaming.cancel')}
-                </Button>
-                <Button type="submit" variant="dark">
-                  {t('modals.renaming.send')}
-                </Button>
-              </div>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      );
-    },
-    removing: (isOpened, onHideHandler, t, handleRemoveChannel) => {
-      return (
-        <Modal show={isOpened} onHide={onHideHandler}>
-          <Modal.Header closeButton>
-            <Modal.Title>{t('modals.removing.title')}</Modal.Title>
-          </Modal.Header>
-
-          <Modal.Body>
-            {t('modals.removing.body')}
-            <div className="d-flex justify-content-between">
+        <Modal.Body>
+          <Form noValidate onSubmit={formik.handleSubmit}>
+            <FormGroup>
+              <FormControl
+                required
+                ref={inputRef}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.body}
+                data-testid="add-channel"
+                isInvalid={formik.errors.body}
+                name="body"
+              />
+              <FormControl.Feedback
+                type="invalid"
+              >
+                {formik.errors.body}
+              </FormControl.Feedback>
+            </FormGroup>
+            <div className="d-flex justify-content-end">
               <Button className="mr-2 btn btn-secondary" onClick={onHideHandler}>
-                {t('modals.removing.cancel')}
+                {t('modals.adding.cancel')}
               </Button>
-              <Button variant="danger" onClick={handleRemoveChannel}>
-                {t('modals.removing.remove')}
+              <Button type="submit" variant="dark">
+                {t('modals.adding.send')}
               </Button>
             </div>
-          </Modal.Body>
-        </Modal>
-      );
-    },
-  };
+          </Form>
+        </Modal.Body>
+      </Modal>
+    );
+  },
+  renaming: (isOpened, onHideHandler, t, formik, inputRef) => {
+    return (
+      <Modal show={isOpened} onHide={onHideHandler}>
+        <Modal.Header closeButton>
+          <Modal.Title>{t('modals.renaming.title')}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form noValidate onSubmit={formik.handleSubmit}>
+            <FormGroup>
+              <FormControl
+                required
+                ref={inputRef}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.body}
+                data-testid="rename-channel"
+                isInvalid={formik.errors.body}
+                name="body"
+              />
+              <FormControl.Feedback
+                type="invalid"
+              >
+                {formik.errors.body}
+              </FormControl.Feedback>
+            </FormGroup>
+            <div className="d-flex justify-content-end">
+              <Button className="mr-2 btn btn-secondary" onClick={onHideHandler}>
+                {t('modals.renaming.cancel')}
+              </Button>
+              <Button type="submit" variant="dark">
+                {t('modals.renaming.send')}
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    );
+  },
+  removing: (isOpened, onHideHandler, t, handleRemoveChannel) => {
+    return (
+      <Modal show={isOpened} onHide={onHideHandler}>
+        <Modal.Header closeButton>
+          <Modal.Title>{t('modals.removing.title')}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          {t('modals.removing.body')}
+          <div className="d-flex justify-content-between">
+            <Button className="mr-2 btn btn-secondary" onClick={onHideHandler}>
+              {t('modals.removing.cancel')}
+            </Button>
+            <Button variant="danger" onClick={handleRemoveChannel}>
+              {t('modals.removing.remove')}
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
+    );
+  },
+};
+
+const CustomModal = ({ modal, onHide }) => {
+  // const renderSettingsByType = {
+  //   adding: (isOpened, onHideHandler, t, formik, inputRef) => {
+  //     return (
+  //       <Modal show={isOpened} onHide={onHideHandler}>
+  //         <Modal.Header closeButton>
+  //           <Modal.Title>{t('modals.adding.title')}</Modal.Title>
+  //         </Modal.Header>
+  //
+  //         <Modal.Body>
+  //           <Form noValidate onSubmit={formik.handleSubmit}>
+  //             <FormGroup>
+  //               <FormControl
+  //                 required
+  //                 ref={inputRef}
+  //                 onChange={formik.handleChange}
+  //                 onBlur={formik.handleBlur}
+  //                 value={formik.values.body}
+  //                 data-testid="add-channel"
+  //                 isInvalid={formik.errors.body}
+  //                 name="body"
+  //               />
+  //               <FormControl.Feedback
+  //                 type="invalid"
+  //               >
+  //                 {formik.errors.body}
+  //               </FormControl.Feedback>
+  //             </FormGroup>
+  //             <div className="d-flex justify-content-end">
+  //               <Button className="mr-2 btn btn-secondary" onClick={onHideHandler}>
+  //                 {t('modals.adding.cancel')}
+  //               </Button>
+  //               <Button type="submit" variant="dark">
+  //                 {t('modals.adding.send')}
+  //               </Button>
+  //             </div>
+  //           </Form>
+  //         </Modal.Body>
+  //       </Modal>
+  //     );
+  //   },
+  //   renaming: (isOpened, onHideHandler, t, formik, inputRef) => {
+  //     return (
+  //       <Modal show={isOpened} onHide={onHideHandler}>
+  //         <Modal.Header closeButton>
+  //           <Modal.Title>{t('modals.renaming.title')}</Modal.Title>
+  //         </Modal.Header>
+  //
+  //         <Modal.Body>
+  //           <Form noValidate onSubmit={formik.handleSubmit}>
+  //             <FormGroup>
+  //               <FormControl
+  //                 required
+  //                 ref={inputRef}
+  //                 onChange={formik.handleChange}
+  //                 onBlur={formik.handleBlur}
+  //                 value={formik.values.body}
+  //                 data-testid="rename-channel"
+  //                 isInvalid={formik.errors.body}
+  //                 name="body"
+  //               />
+  //               <FormControl.Feedback
+  //                 type="invalid"
+  //               >
+  //                 {formik.errors.body}
+  //               </FormControl.Feedback>
+  //             </FormGroup>
+  //             <div className="d-flex justify-content-end">
+  //               <Button className="mr-2 btn btn-secondary" onClick={onHideHandler}>
+  //                 {t('modals.renaming.cancel')}
+  //               </Button>
+  //               <Button type="submit" variant="dark">
+  //                 {t('modals.renaming.send')}
+  //               </Button>
+  //             </div>
+  //           </Form>
+  //         </Modal.Body>
+  //       </Modal>
+  //     );
+  //   },
+  //   removing: (isOpened, onHideHandler, t, handleRemoveChannel) => {
+  //     return (
+  //       <Modal show={isOpened} onHide={onHideHandler}>
+  //         <Modal.Header closeButton>
+  //           <Modal.Title>{t('modals.removing.title')}</Modal.Title>
+  //         </Modal.Header>
+  //
+  //         <Modal.Body>
+  //           {t('modals.removing.body')}
+  //           <div className="d-flex justify-content-between">
+  //             <Button className="mr-2 btn btn-secondary" onClick={onHideHandler}>
+  //               {t('modals.removing.cancel')}
+  //             </Button>
+  //             <Button variant="danger" onClick={handleRemoveChannel}>
+  //               {t('modals.removing.remove')}
+  //             </Button>
+  //           </div>
+  //         </Modal.Body>
+  //       </Modal>
+  //     );
+  //   },
+  // };
   const inputRef = useRef();
   useEffect(() => {
     switch (modal.type) {
@@ -189,7 +291,7 @@ const CustomModal = ({ modal, onHide }) => {
   };
   const formik = GetFormik(modal);
   const render = renderSettingsByType[modal.type] ?? (() => null);
-  CustomModal.displayName = 'CustomModal';
+
   return render(modal.isOpened, onHide, t, formik, inputRef);
 };
 
